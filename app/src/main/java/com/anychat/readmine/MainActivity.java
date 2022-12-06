e198826c05ba883812dd04dcc39c62c14af1b3d9
anychatDeokgi <anychatliveforownhappiness@gmail.com>
2022-12-06 14:54:46 +0900

    splash 숨김

--- app/src/main/java/com/anychat/readmine/MainActivity.java
+++ app/src/main/java/com/anychat/readmine/MainActivity.java
@@ -3,6 +3,8 @@ package com.anychat.readmine;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewTreeObserver;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -17,10 +19,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //스플래시 숨김
+        final View content = findViewById(android.R.id.content);
+        content.getViewTreeObserver().addOnPreDrawListener(
+                new ViewTreeObserver.OnPreDrawListener() {
+                    @Override
+                    public boolean onPreDraw() {
+                        // Check if the initial data is ready.
+                        if (false) {
+                            // The content is ready; start drawing.
+                            content.getViewTreeObserver().removeOnPreDrawListener(this);
+                            return true;
+                        } else {
+                            // The content is not ready; suspend.
+                            return false;
+                        }
+                    }
+                });
+
+        //스플래시 없어질때 애니메이션
+
+
+        // 웹뷰
         web = findViewById(R.id.webview);
 
+        Log.d("Finished ::::", "Hey :::::;");
         try{
-            web.loadUrl("https://demoqa.anychat.com:3000/projects/test-project/issues/gantt?utf8=%E2%9C%93&set_filter=1&gantt=1&f%5B%5D=status_id&op%5Bstatus_id%5D=%3D&v%5Bstatus_id%5D%5B%5D=5&f%5B%5D=&query%5Bdraw_selected_columns%5D=0&query%5Bdraw_relations%5D=0&query%5Bdraw_relations%5D=1&query%5Bdraw_progress_line%5D=0&months=2&month=12&year=2022&zoom=2");
+            String url = "https://demoqa.anychat.com:3000/projects/test-project/issues/gantt?utf8=%E2%9C%93&set_filter=1&gantt=1&f%5B%5D=status_id&op%5Bstatus_id%5D=%3D&v%5Bstatus_id%5D%5B%5D=5&f%5B%5D=&query%5Bdraw_selected_columns%5D=0&query%5Bdraw_relations%5D=0&query%5Bdraw_relations%5D=1&query%5Bdraw_progress_line%5D=0&months=2&month=12&year=2022&zoom=2";
+            web.loadUrl(url);
             web.setWebViewClient(new WebViewClient()); // 현재 앱을 나가서 새로운 브라우저를 열지 않도록 함.
             mWebSettings = web.getSettings(); // 웹뷰에서 webSettings를 사용할 수 있도록 함.
             mWebSettings.setJavaScriptEnabled(true); //웹뷰에서 javascript를 사용하도록 설정
