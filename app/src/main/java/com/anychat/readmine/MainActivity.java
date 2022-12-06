e6ab2d429df4d0b4de0edcca26d0bbbcea44d7d4
anychatDeokgi <anychatliveforownhappiness@gmail.com>
2022-12-06 15:35:53 +0900

    progress bar

--- app/src/main/java/com/anychat/readmine/MainActivity.java
+++ app/src/main/java/com/anychat/readmine/MainActivity.java
@@ -1,5 +1,6 @@
 package com.anychat.readmine;
 
+import android.graphics.Bitmap;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -8,13 +9,14 @@ import android.view.ViewTreeObserver;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
-import android.widget.Button;
+import android.widget.ProgressBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView web;
     private WebSettings mWebSettings;
-    Button btn;
+    private ProgressBar spinner;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +29,7 @@ public class MainActivity extends AppCompatActivity {
                     @Override
                     public boolean onPreDraw() {
                         // Check if the initial data is ready.
-                        if (false) {
+                        if (true) {
                             // The content is ready; start drawing.
                             content.getViewTreeObserver().removeOnPreDrawListener(this);
                             return true;
@@ -41,19 +43,24 @@ public class MainActivity extends AppCompatActivity {
         //스플래시 없어질때 애니메이션
 
 
+        //커스텀 웹뷰
+
         // 웹뷰
         web = findViewById(R.id.webview);
 
         Log.d("Finished ::::", "Hey :::::;");
         try{
             String url = "https://demoqa.anychat.com:3000/projects/test-project/issues/gantt?utf8=%E2%9C%93&set_filter=1&gantt=1&f%5B%5D=status_id&op%5Bstatus_id%5D=%3D&v%5Bstatus_id%5D%5B%5D=5&f%5B%5D=&query%5Bdraw_selected_columns%5D=0&query%5Bdraw_relations%5D=0&query%5Bdraw_relations%5D=1&query%5Bdraw_progress_line%5D=0&months=2&month=12&year=2022&zoom=2";
-            web.loadUrl(url);
-            web.setWebViewClient(new WebViewClient()); // 현재 앱을 나가서 새로운 브라우저를 열지 않도록 함.
+
+            web.setWebViewClient(new CustomWebViewClient()); // 현재 앱을 나가서 새로운 브라우저를 열지 않도록 함.
             mWebSettings = web.getSettings(); // 웹뷰에서 webSettings를 사용할 수 있도록 함.
             mWebSettings.setJavaScriptEnabled(true); //웹뷰에서 javascript를 사용하도록 설정
             mWebSettings.setTextZoom(80);
+            spinner = (ProgressBar)findViewById(R.id.progressBar1);
+            web.loadUrl(url);
 
         /*
+        web.setWebViewClient(new WebViewClient()); // 현재 앱을 나가서 새로운 브라우저를 열지 않도록 함.
         mWebSettings.setJavaScriptCanOpenWindowsAutomatically(false); //멀티윈도우 띄우는 것
         mWebSettings.setAllowFileAccess(true); //파일 엑세스
         mWebSettings.setLoadWithOverviewMode(true); // 메타태그
@@ -70,6 +77,22 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
+    private class CustomWebViewClient extends WebViewClient {
+
+        @Override
+        public void onPageStarted(WebView webview, String url, Bitmap favicon) {
+            webview.setVisibility(webview.INVISIBLE);
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+
+            spinner.setVisibility(View.GONE);
+            view.setVisibility(web.VISIBLE);
+            super.onPageFinished(view, url);
+
+        }
+    }
     @Override
     protected void onDestroy() {
         super.onDestroy();
@@ -78,5 +101,6 @@ public class MainActivity extends AppCompatActivity {
             web=null; // remove webView, prevent chromium to crash
         }
     }
+
 
 }
